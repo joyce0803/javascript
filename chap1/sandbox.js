8ce784f3821283dcfabd7157ba5ce5c097299b9b
joyce08hcl <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>
2024-12-18 06:32:24 +0000

    done

--- chap1/sandbox.js
+++ chap1/sandbox.js
@@ -1,1 +1,9 @@
-console
+const pList = document.querySelectorAll('p');
+console.log(pList)
+pList.forEach(line => {
+    console.log(line.innerText);
+    eachLine = line.innerText
+    if(eachLine.includes('error')){
+        line.setAttribute('style',"")
+    }
+});
